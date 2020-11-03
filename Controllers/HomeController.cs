714dd96c2d31f3127f14c445ead85f898ef6f41f
Pieter de Bruin <pdebruin@hotmail.com>
2020-11-03 13:28:28 +0100

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Graph;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using _2_1_Call_MSGraph.Models;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _2_1_Call_MSGraph.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public HomeController(ILogger<HomeController> logger,
+                          GraphServiceClient graphServiceClient)
+        {
+            _logger = logger;
+            _graphServiceClient = graphServiceClient;
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["ApiResult"] = user.DisplayName;
+
+            var groups = await _graphServiceClient.Me.MemberOf.Request().GetAsync();
+            ViewData["groups"] = groups;
+
+            var applications = await _graphServiceClient.Me.AppRoleAssignments.Request().GetAsync();
+            ViewData["applications"] = applications;
+
+            return View();
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> Profile()
+        {
+            var me = await _graphServiceClient.Me.Request().GetAsync();
+            ViewData["Me"] = me;
+
+            try
+            {
+                // Get user photo
+                using (var photoStream = await _graphServiceClient.Me.Photo.Content.Request().GetAsync())
+                {
+                    byte[] photoByte = ((MemoryStream)photoStream).ToArray();
+                    ViewData["Photo"] = Convert.ToBase64String(photoByte);
+                }
+            }
+            catch (System.Exception)
+            {
+                ViewData["Photo"] = null;
+            }
+
+            return View();
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> MyGroups()
+        {
+            var mygroups = await _graphServiceClient.Me.MemberOf.Request().GetAsync();
+
+            ViewData["mygroups"] = mygroups;
+
+            return View();
+        }
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> MyUsers()
+        {
+            var myusers = await _graphServiceClient.Groups[groupid].Members.Request().GetAsync();
+
+            ViewData["myusers"] = myusers;
+
+            return View();
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> MyApps()
+        {
+            var myapps = await _graphServiceClient.Me.AppRoleAssignments.Request().GetAsync();
+            ViewData["myapps"] = myapps;
+
+            return View();
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> ManageGroups()
+        {
+            if (await IsAuthorized("ManageGroups"))
+            {
+                var transitivegroups = await _graphServiceClient.Me.TransitiveMemberOf.Request().GetAsync();
+
+                HomeHelper helper = new HomeHelper();
+                SortedDictionary<string, string> groups = new SortedDictionary<string, string>();
+                ViewData["transitivegroups"] = helper.TransitiveGroupsToDictionary(groups, transitivegroups);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "DownstreamApi:Scopes")]
+        public async Task<IActionResult> ManageUsers()
+        {
+            if (await IsAuthorized("ManageUsers"))
+            {
+                var transitivegroups = await _graphServiceClient.Me.TransitiveMemberOf.Request().GetAsync();
+
+                HomeHelper helper = new HomeHelper();
+                SortedDictionary<string, string> groups = new SortedDictionary<string, string>();
+                groups = helper.TransitiveGroupsToDictionary(groups, transitivegroups);
+
+                SortedDictionary<string, string> members = await GetGroupMembers(groups);
+                ViewData["members"] = members;
+            }
+            else {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        private async Task<SortedDictionary<string, string>> GetGroupMembers(SortedDictionary<string, string> groups)
+        {
+            //https://docs.microsoft.com/en-us/graph/api/group-list-members
+            SortedDictionary<string, string> users = new SortedDictionary<string, string>();
+            HomeHelper helper = new HomeHelper();
+            foreach (string s in groups.Values)
+            {
+                var members = await _graphServiceClient.Groups[s].Members
+                    .Request()
+                    .GetAsync();
+
+                users = helper.UsersToDictionary(users, members);
+            }
+
+            return users;
+        }
+
+        private async Task<bool> IsAuthorized(string action)
+        {
+            bool result = false;
+            var mygroups = await _graphServiceClient.Me.MemberOf.Request().GetAsync();
+
+            HomeHelper helper = new HomeHelper();
+            result = helper.IsAdmin(mygroups);
+
+            return result;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Snippets/HomeHelper.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2_1_Call_MSGraph.Controllers
+{
+    public class HomeHelper
+    {
+        public bool IsAdmin(Microsoft.Graph.IUserMemberOfCollectionWithReferencesPage mygroups)
+        {
+            bool result = false;
+
+            SortedDictionary<string, string> groups = new SortedDictionary<string, string>();
+
+            groups = GroupsToDictionary(groups, mygroups);
+
+            foreach (KeyValuePair<string, string> kvp in groups)
+            {
+                if (kvp.Key.Contains("_admin"))
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public SortedDictionary<string, string> GroupsToDictionary(SortedDictionary<string, string> groups, Microsoft.Graph.IUserMemberOfCollectionWithReferencesPage mygroups)
+        {
+            foreach (var group in mygroups)
+            {
+                var properties = group.GetType().GetProperties();
+                string name = "";
+                string id = "";
+
+                foreach (var child in properties)
+                {
+                    if (child.Name == "DisplayName" || child.Name == "Id")
+                    {
+
+                        object value = child.GetValue(group);
+                        string stringRepresentation;
+                        if (!(value is string) && value is IEnumerable<string>)
+                        {
+                            stringRepresentation = "["
+                                + string.Join(", ", (value as IEnumerable<string>).OfType<object>().Select(c => c.ToString()))
+                                + "]";
+                        }
+                        else
+                        {
+                            stringRepresentation = value?.ToString();
+                        }
+
+                        if (child.Name == "DisplayName")
+                        {
+                            name = stringRepresentation;
+                        }
+                        else if (child.Name == "Id")
+                        {
+                            id = stringRepresentation;
+                        }
+                    }
+                }
+
+                if (name != "" && id != "")
+                {
+                    if (!groups.ContainsKey(name))
+                    {
+                        groups.Add(name, id);
+                    }
+                }
+            }
+
+            return groups;
+        }
+
+        public SortedDictionary<string, string> TransitiveGroupsToDictionary(SortedDictionary<string, string> groups, Microsoft.Graph.IUserTransitiveMemberOfCollectionWithReferencesPage transitivegroups)
+        {
+            foreach (var group in transitivegroups)
+            {
+                var properties = group.GetType().GetProperties();
+                string name = "";
+                string id = "";
+
+                foreach (var child in properties)
+                {
+                    if (child.Name == "DisplayName" || child.Name == "Id")
+                    {
+
+                        object value = child.GetValue(group);
+                        string stringRepresentation;
+                        if (!(value is string) && value is IEnumerable<string>)
+                        {
+                            stringRepresentation = "["
+                                + string.Join(", ", (value as IEnumerable<string>).OfType<object>().Select(c => c.ToString()))
+                                + "]";
+                        }
+                        else
+                        {
+                            stringRepresentation = value?.ToString();
+                        }
+
+                        if (child.Name == "DisplayName")
+                        {
+                            name = stringRepresentation;
+                        }
+                        else if (child.Name == "Id")
+                        {
+                            id = stringRepresentation;
+                        }
+                    }
+                }
+
+                if (name != "" && id != "")
+                {
+                    if (!groups.ContainsKey(name))
+                    {
+                        groups.Add(name, id);
+                    }
+                }
+            }
+
+            return groups;
+        }
+        public SortedDictionary<string, string> UsersToDictionary(SortedDictionary<string, string> users, Microsoft.Graph.IGroupMembersCollectionWithReferencesPage members)
+        {
+            foreach (var member in members)
+            {
+                if (member.GetType().ToString() == "Microsoft.Graph.User")
+                {
+                    var properties = member.GetType().GetProperties();
+                    string name = "";
+                    string id = "";
+
+                    foreach (var child in properties)
+                    {
+                        if (child.Name == "DisplayName" || child.Name == "Id")
+                        {
+
+                            object value = child.GetValue(member);
+                            string stringRepresentation;
+                            if (!(value is string) && value is IEnumerable<string>)
+                            {
+                                stringRepresentation = "["
+                                    + string.Join(", ", (value as IEnumerable<string>).OfType<object>().Select(c => c.ToString()))
+                                    + "]";
+                            }
+                            else
+                            {
+                                stringRepresentation = value?.ToString();
+                            }
+
+                            if (child.Name == "DisplayName")
+                            {
+                                name = stringRepresentation;
+                            }
+                            else if (child.Name == "Id")
+                            {
+                                id = stringRepresentation;
+                            }
+                        }
+                    }
+
+                    if (name != "" && id != "")
+                    {
+                        if (!users.ContainsKey(name))
+                        {
+                            users.Add(name, id);
+                        }
+                    }
+                }
+            }
+
+            return users;
+        }
+
+    }
+}
